db09f9e7b230a7c289ff89a2c2ebd9d24407c1e9
jose.esmoris <jose.esmoris@globant.com>
2022-03-22 10:48:01 -0500

    ALSO READS LISTS

--- web/base_page.py
+++ web/base_page.py
@@ -1,9 +1,7 @@
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+
 
 class BasePage:
     def __init__(self, url: str):
